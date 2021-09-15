84739e57385054874fa241af2f9191018fac2ae3
Brandon Wharton <whartonbm@gmail.com>
2021-09-14 19:02:50 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# A tic-tac-toe game
+gameboard = {
+    'top_row': [' ', ' ', ' '],
+    'middle_row': [' ', ' ', ' '],
+    'bottom_row': [' ', ' ', ' ']
+}
+
+def run_game():
+
+
+    player1 = input('Player 1, input your name', )
+    player2 = input('Player 2, input your name', )
