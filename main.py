bd85142e60524152ce64bfe149a3cfb94fa59251
Brandon Wharton <whartonbm@gmail.com>
2021-09-14 19:31:28 -0500

    Updated venv to Python 3

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ gameboard = {
 }
 
 def run_game():
-
-
+    global gameboard
     player1 = input('Player 1, input your name', )
     player2 = input('Player 2, input your name', )
+    print(f'Hello {player1} and {player2} and welcome to tic-tac-toe.')
+
+run_game()
